2fc25af23becdd3b349cbb12847c20ca3e53b62e
TomP22 <to.penap@duocuc.cl>
2026-06-25 03:48:07 -0400

    refactor: corregir rutas base y configuracion de secreto en RestClientConfig

--- src/main/java/cl/municipalidad/pagos/config/RestClientConfig.java
+++ src/main/java/cl/municipalidad/pagos/config/RestClientConfig.java
@@ -7,12 +7,14 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
+
+    private final String GATEWAY_URL = "http://localhost:8080";
     private final String GATEWAY_SECRET = "ClaveUltraSecretaEInviolableParaLaMunicipalidad2026!";
 
     @Bean(name = "restClientCancha")
     public RestClient restClientCancha() {
         return RestClient.builder()
-                .baseUrl("http://localhost:8083")
+                .baseUrl(GATEWAY_URL)
                 .requestInterceptor(new JwtInterceptor())
                 .defaultHeader("X-Gateway-Secret", GATEWAY_SECRET) 
                 .build();
@@ -21,7 +23,7 @@ public class RestClientConfig {
     @Bean(name = "restClientReservas")
     public RestClient restClientReservas() {
         return RestClient.builder()
-                .baseUrl("http://localhost:8084")
+                .baseUrl(GATEWAY_URL)
                 .requestInterceptor(new JwtInterceptor())
                 .defaultHeader("X-Gateway-Secret", GATEWAY_SECRET)
                 .build();
